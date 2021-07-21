f944ee9ebd0630037e2fc68da8e554fbafc66278
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-07-21 19:24:29 +0900

    reset

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
           options = {{headerShown: false}}
         />
         <Stack.Screen 
-          name="Timer" 
+          name="ATimer" 
           component={AnimationTimer}
           options = {{headerShown: false}}
         />
